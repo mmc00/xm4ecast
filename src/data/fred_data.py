7add3a1457830fdedc28f97e46542c7ded4f7947
mmc00 <dracomarmol@gmail.com>
2022-08-23 15:14:28 -0600

    fred data

---
+++ src/data/fred_data.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def cleaning_fred(ds, var_name):
+    """
+    Cleaning data series (ds)
+    """
+    dt = ds.to_frame().reset_index()
+    dt = (dt.rename(columns={dt.columns[0] : "date",
+                             dt.columns[1] : "value"})
+            .assign(variable = var_name)
+         )
+    return dt
