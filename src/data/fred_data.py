5aebc52eb45a63c019de7f38df56ac06725ca23f
mmc00 <dracomarmol@gmail.com>
2022-09-15 14:09:17 -0600

    fixing name variable

--- src/data/fred_data.py
+++ src/data/fred_data.py
@@ -7,6 +7,6 @@ def cleaning_fred(ds, var_name):
     dt = ds.to_frame().reset_index()
     dt = (dt.rename(columns={dt.columns[0] : "date",
                              dt.columns[1] : "value"})
-            .assign(variable = var_name)
+            .assign(var = var_name)
          )
     return dt
